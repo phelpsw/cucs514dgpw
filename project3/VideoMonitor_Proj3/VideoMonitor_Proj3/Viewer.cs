96a39aa92d1428f6d466186392c2d27aa1eed3af
phelpsw <phelpsw@62809423-b64b-0410-b5a8-b3a0f5d1cecd>
2008-05-02 00:53:07 +0000

    initial class structure for project 3

---
+++ project3/VideoMonitor_Proj3/VideoMonitor_Proj3/Viewer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VideoMonitor_Proj3
+{
+    public partial class Viewer : UserControl
+    {
+        public Viewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
