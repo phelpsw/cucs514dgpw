73d7b4bf0f0096e20960b6fcea0110b31accdd2b
phelpsw <phelpsw@62809423-b64b-0410-b5a8-b3a0f5d1cecd>
2008-05-05 22:27:59 +0000

    Endpoints now defined properly based on class structure modifications

--- project3/VideoMonitor_Proj3/VideoMonitor_Proj3/VideoServer.cs
+++ project3/VideoMonitor_Proj3/VideoMonitor_Proj3/VideoServer.cs
@@ -7,66 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+// IOutgoing = IVMAppFunc
+// IIncoming = IVMCommInt
+// IBank = IVideoStream
+
 namespace VideoMonitor_Proj3
 {
     [QS.Fx.Reflection.ComponentClass("2`1", "VideoServer", "This component provides a centralized source index and source distributor")]
-    public partial class VideoServer : UserControl, QS.Fx.Object.Classes.IUI,
-        QS.Fx.Interface.Classes.ICheckpointedCommunicationChannelClient<VMMessage, ViewerState>,
-        QS.Fx.Interface.Classes.ICheckpointedCommunicationChannelClient<VMMessage, SourceState>
+    public partial class VideoServer : 
+        UserControl, 
+        QS.Fx.Object.Classes.IUI,
+        IVMAppFunc
     {
         public VideoServer(
-            [QS.Fx.Reflection.Parameter("sourceChannel", QS.Fx.Reflection.ParameterClass.Value)]
-            QS.Fx.Object.IReference<QS.Fx.Object.Classes.ICheckpointedCommunicationChannel<VMMessage, SourceState>> sourceChannel,
-            [QS.Fx.Reflection.Parameter("viewChannel", QS.Fx.Reflection.ParameterClass.Value)]
-            QS.Fx.Object.IReference<QS.Fx.Object.Classes.ICheckpointedCommunicationChannel<VMMessage, ViewerState>> viewChannel)
+            [QS.Fx.Reflection.Parameter("sourceStreamProcessor", QS.Fx.Reflection.ParameterClass.Value)] 
+            QS.Fx.Object.IReference<IVideoStream> sourceStreamProcessor,
+            [QS.Fx.Reflection.Parameter("viewerStreamProcessor", QS.Fx.Reflection.ParameterClass.Value)] 
+            QS.Fx.Object.IReference<IVideoStream> viewerStreamProcessor)
         {
             InitializeComponent();
-            this.internal_endpoint = QS.Fx.Endpoint.Internal.Create.ExportedUI(this);
+            this.uiendpoint = QS.Fx.Endpoint.Internal.Create.ExportedUI(this);
+            this.sourceStreamEndPoint = QS.Fx.Endpoint.Internal.Create.DualInterface<IVMCommInt, IVMAppFunc>(this);
+            this.viewerStreamEndPoint = QS.Fx.Endpoint.Internal.Create.DualInterface<IVMCommInt, IVMAppFunc>(this);
+            this.sourceConnection = this.sourceStreamEndPoint.Connect(sourceStreamProcessor.Object.VideoProcessor);
+            this.viewerConnection = this.viewerStreamEndPoint.Connect(viewerStreamProcessor.Object.VideoProcessor);
         }
 
-        private QS.Fx.Endpoint.Internal.IExportedUI internal_endpoint;
+        private QS.Fx.Endpoint.Internal.IExportedUI uiendpoint;
+        private QS.Fx.Endpoint.Internal.IDualInterface<IVMCommInt, IVMAppFunc> sourceStreamEndPoint;
+        private QS.Fx.Endpoint.Internal.IDualInterface<IVMCommInt, IVMAppFunc> viewerStreamEndPoint;
+        private QS.Fx.Endpoint.IConnection sourceConnection;
+        private QS.Fx.Endpoint.IConnection viewerConnection;
+        
 
         #region IUI Members
 
         QS.Fx.Endpoint.Classes.IExportedUI QS.Fx.Object.Classes.IUI.UI
         {
-            get { return this.internal_endpoint; }
+            get { return this.uiendpoint; }
         }
 
         #endregion
 
-        #region ICheckpointedCommunicationChannelClient<VMMessage,ViewerState> Members
 
-        ViewerState QS.Fx.Interface.Classes.ICheckpointedCommunicationChannelClient<VMMessage, ViewerState>.Checkpoint()
+        #region IVMAppFunc Members
+
+        void IVMAppFunc.Ready()
         {
             throw new NotImplementedException();
         }
 
-        void QS.Fx.Interface.Classes.ICheckpointedCommunicationChannelClient<VMMessage, ViewerState>.Initialize(ViewerState _checkpoint)
+        void IVMAppFunc.RecieveFrame(Image frame, FrameID id)
         {
             throw new NotImplementedException();
         }
 
-        void QS.Fx.Interface.Classes.ICheckpointedCommunicationChannelClient<VMMessage, ViewerState>.Receive(VMMessage _message)
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion
-
-        #region ICheckpointedCommunicationChannelClient<VMMessage,SourceState> Members
-
-        SourceState QS.Fx.Interface.Classes.ICheckpointedCommunicationChannelClient<VMMessage, SourceState>.Checkpoint()
-        {
-            throw new NotImplementedException();
-        }
-
-        void QS.Fx.Interface.Classes.ICheckpointedCommunicationChannelClient<VMMessage, SourceState>.Initialize(SourceState _checkpoint)
-        {
-            throw new NotImplementedException();
-        }
-
-        void QS.Fx.Interface.Classes.ICheckpointedCommunicationChannelClient<VMMessage, SourceState>.Receive(VMMessage _message)
+        void IVMAppFunc.RecieveCommand(VMAddress src, string rfc_command, Parameter[] parameters)
         {
             throw new NotImplementedException();
         }
--- project3/VideoMonitor_Proj3/VideoMonitor_Proj3/VideoViewer.cs
+++ project3/VideoMonitor_Proj3/VideoMonitor_Proj3/VideoViewer.cs
@@ -10,41 +10,47 @@ using System.Windows.Forms;
 namespace VideoMonitor_Proj3
 {
     [QS.Fx.Reflection.ComponentClass("3`1", "VideoViewer", "This component provides an interface to view selected indexed video sources")]
-    public partial class VideoViewer : UserControl, QS.Fx.Object.Classes.IUI,
-        QS.Fx.Interface.Classes.ICheckpointedCommunicationChannelClient<VMMessage, ViewerState>
+    public partial class VideoViewer : 
+        UserControl, 
+        QS.Fx.Object.Classes.IUI,
+        IVMAppFunc
     {
         public VideoViewer(
-            [QS.Fx.Reflection.Parameter("channel", QS.Fx.Reflection.ParameterClass.Value)]
-            QS.Fx.Object.IReference<QS.Fx.Object.Classes.ICheckpointedCommunicationChannel<VMMessage, ViewerState>> channel)
+            [QS.Fx.Reflection.Parameter("streamProcessor", QS.Fx.Reflection.ParameterClass.Value)] 
+            QS.Fx.Object.IReference<IVideoStream> streamProcessor)
         {
             InitializeComponent();
-            this.internal_endpoint = QS.Fx.Endpoint.Internal.Create.ExportedUI(this);
+            this.uiendpoint = QS.Fx.Endpoint.Internal.Create.ExportedUI(this);
+            this.streamEndPoint = QS.Fx.Endpoint.Internal.Create.DualInterface<IVMCommInt, IVMAppFunc>(this);
+            this.viewerConnection = this.streamEndPoint.Connect(streamProcessor.Object.VideoProcessor);
         }
 
-        private QS.Fx.Endpoint.Internal.IExportedUI internal_endpoint;
+        private QS.Fx.Endpoint.Internal.IExportedUI uiendpoint;
+        private QS.Fx.Endpoint.Internal.IDualInterface<IVMCommInt, IVMAppFunc> streamEndPoint;
+        private QS.Fx.Endpoint.IConnection viewerConnection;
 
         #region IUI Members
 
         QS.Fx.Endpoint.Classes.IExportedUI QS.Fx.Object.Classes.IUI.UI
         {
-            get { return this.internal_endpoint; }
+            get { return this.uiendpoint; }
         }
 
         #endregion
 
-        #region ICheckpointedCommunicationChannelClient<VMMessage,ViewerState> Members
+        #region IVMAppFunc Members
 
-        ViewerState QS.Fx.Interface.Classes.ICheckpointedCommunicationChannelClient<VMMessage, ViewerState>.Checkpoint()
+        void IVMAppFunc.Ready()
         {
             throw new NotImplementedException();
         }
 
-        void QS.Fx.Interface.Classes.ICheckpointedCommunicationChannelClient<VMMessage, ViewerState>.Initialize(ViewerState _checkpoint)
+        void IVMAppFunc.RecieveFrame(Image frame, FrameID id)
         {
             throw new NotImplementedException();
         }
 
-        void QS.Fx.Interface.Classes.ICheckpointedCommunicationChannelClient<VMMessage, ViewerState>.Receive(VMMessage _message)
+        void IVMAppFunc.RecieveCommand(VMAddress src, string rfc_command, Parameter[] parameters)
         {
             throw new NotImplementedException();
         }
