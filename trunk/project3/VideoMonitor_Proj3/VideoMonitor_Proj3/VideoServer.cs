d2de14710708cc8277ea8bfc406fedc58078297a
phelpsw <phelpsw@62809423-b64b-0410-b5a8-b3a0f5d1cecd>
2008-05-06 03:21:01 +0000

    ValueClass numbering fix, parameters renamed to VMParameters

--- trunk/project3/VideoMonitor_Proj3/VideoMonitor_Proj3/VideoServer.cs
+++ trunk/project3/VideoMonitor_Proj3/VideoMonitor_Proj3/VideoServer.cs
@@ -58,7 +58,7 @@ namespace VideoMonitor_Proj3
                 viewerStreamEndPoint.Interface.SendFrame(frame, id);
         }
 
-        void IVMAppFunc.RecieveCommand(VMAddress src, string rfc_command, Parameter[] parameters, string origID)
+        void IVMAppFunc.RecieveCommand(VMAddress src, string rfc_command, VMParameter[] parameters, string origID)
         {
             throw new NotImplementedException();
         }
